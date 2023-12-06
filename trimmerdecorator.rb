527e429a049fe6ddaf5e3af43441f1479b13c188
jcmunav63 <jcmunav63@gmail.com>
2023-12-06 09:08:48 -0600

    Edit TrimmerDecorator class to correct a required change.

--- trimmerdecorator.rb
+++ trimmerdecorator.rb
@@ -4,6 +4,6 @@ require_relative 'decorator'
 class TrimmerDecorator < Decorator
   def correct_name
     original_name = nameable.correct_name
-    original_name.length > 10 ? original_name[0, 9] : original_name
+    original_name.length > 10 ? original_name[0..9] : original_name
   end
 end
