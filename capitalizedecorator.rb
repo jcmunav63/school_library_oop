fca81037578dcf1ba69dd0cc641668ea8ec5d447
jcmunav63 <jcmunav63@gmail.com>
2023-12-06 08:52:42 -0600

    Edit Decorator & TrimmerDecorator class to correct required changes.

--- capitalizedecorator.rb
+++ capitalizedecorator.rb
@@ -3,6 +3,6 @@ require_relative 'decorator'
 # It capitalizes the name string
 class CapitalizeDecorator < Decorator
   def correct_name
-    @nameable.correct_name.capitalize
+    nameable.correct_name.capitalize
   end
 end
--- decorator.rb
+++ decorator.rb
@@ -2,12 +2,14 @@ require_relative 'nameable'
 
 # A decorator class with a correct_name method
 class Decorator < Nameable
+  attr_accessor :nameable
+
   def initialize(nameable)
     super()
-    @nameable = nameable
+    self.nameable = nameable
   end
 
   def correct_name
-    @nameable.correct_name
+    nameable.correct_name
   end
 end
