8522ff3006cde2a8d4e9cf01c44018b4968061f1
jcmunav63 <jcmunav63@gmail.com>
2023-12-08 14:47:49 -0600

    Prepare a bug for this exercise.

--- book.rb
+++ book.rb
@@ -3,7 +3,7 @@ require_relative 'rental'
 class Book
   attr_accessor :title, :author, :rentals
 
-  def initialize(title, author)
+  def initialize(title: 'Unknown', author: 'Unknown')
     @title = title
     @author = author
     @rentals = []
